3f1d83457e0941c2fa0e9104fb12e9a0952a4407
Yor-in <119989089+Yor-in@users.noreply.github.com>
2023-08-31 12:13:54 +0700

    Add files via upload

---
+++ GUIAssignment/src/main/java/com/mycompany/guiassignment/GUIAssignment.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.guiassignment;
+
+/**
+ *
+ * @author CLASSROOM
+ */
+public class GUIAssignment {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
